e242ee774f29bfd1e91eb7ce2c4e359cdb44225d
Jason Lu <jhlu87@yahoo.com>
2017-01-09 10:42:35 -0500

    changed to overwrite all data daily

--- main.py
+++ main.py
@@ -12,31 +12,17 @@ import datetime
 
 def update_data(sym,source):
     file_sym = sym.replace('^','')
-    try:
-        df1 = pd.read_csv('%s.csv'%file_sym)
-        start = datetime.datetime.strptime(df1.iloc[-1].Date,'%Y-%m-%d') + \
-                datetime.timedelta(days=1)
-        print('Previous data found. Appending all new data')
-    except FileNotFoundError:
-        print('File does not exist, gathering all data and creating new file')  
-        df1 = pd.DataFrame()
-        start = datetime.datetime(1900,1,30)
+    start = datetime.datetime(1900,1,30)
     end = None
-    if start.date() != datetime.datetime.now().date():
-        if not _get_cboe_url(sym):
-            df = web.DataReader(sym,source,start,end).round(2)
-        else:
-            df = _get_cboe_data(sym)
-            if not df1.empty: 
-                df = df[df.index > df1.iloc[-1,0]]
-        if not df.empty:
-            if df1.empty:
-                df.to_csv('%s.csv'%file_sym)
-            else:
-                df.to_csv('%s.csv'%file_sym,mode='a',header=None)
-        print('%s updated' % file_sym)
+    if not _get_cboe_url(sym):
+        df = web.DataReader(sym,source,start,end).round(2)
     else:
-        print('%s already up to date. Skipping' % file_sym)
+        df = _get_cboe_data(sym)
+    if not df.empty:
+        df.to_csv('%s.csv'%file_sym)
+    else:
+        print('Error: no data for %s' % file_sym)
+    print('%s updated' % file_sym)
 
 def _get_cboe_data(sym):
     df = pd.read_csv(_get_cboe_url(sym),skiprows=3)
@@ -52,7 +38,7 @@ def _get_cboe_url(sym):
         return None
             
 if __name__=='__main__':
-    syms = ['VXX','^VIX','^VXV','^VXST','SPY']
+    syms = ['VXX','^VIX','^VXV','^VXST','SPY','^SP500TR']
     src = 'yahoo'
     for sym in syms:
         update_data(sym,src)
